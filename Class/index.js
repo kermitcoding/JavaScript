bc27d5703a604d4ac71361a3ebedeeb49dc20564
kermitcoding <kermit1024@outlook.com>
2024-04-11 22:47:05 +0800

    update

--- Class/index.js
+++ Class/index.js
@@ -1,7 +1,26 @@
 let btn1 = document.getElementById("p1-btn")
 btn1.addEventListener("click", function () {
     let div1 = document.getElementById("text");
-    div1.style.color = "red";
+    switch (div1.style.color) {
+        case "black":
+            div1.style.color = "red";
+            break;
+        case "red":
+            div1.style.color = "blue";
+            break;
+        case "blue":
+            div1.style.color = "pink";
+            break;
+        case "pink":
+            div1.style.color = "black";
+            break;
+        default:
+            div1.style.color = "red";
+            break;
+    }
+
+
+
 })
 
 let btn2 = document.getElementById("p2-btn");
